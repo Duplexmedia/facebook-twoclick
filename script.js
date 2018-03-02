abf121a7930dc303baf11c5e056ba2ef657ac1f8
IT4need, Jan Andrè Schlösser <info@it4need.de>
2018-03-02 10:52:21 +0100

    :bug: Script tag needs to be escaped

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 								js = d.createElement(s); js.id = id;
 								js.src = 'https://connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.11&appId=214947809010830';
 								fjs.parentNode.insertBefore(js, fjs);
-						}(document, 'script', 'facebook-jssdk'));</script>
+						}(document, 'script', 'facebook-jssdk'));<\/script>
 
 				<div class="fb-like" data-href="${document.location.href}" data-layout="button"
 						 data-action="like" data-size="small" data-show-faces="false" data-share="false"></div></div>`)
